ffe9aadfdd8f17d2fac6327c7c206f922d803194
Zahin Nambiar <znambi@utexas.edu>
2019-05-02 23:47:21 -0500

    inserted boolean variable to track whether both nodes x and y values have been assigned. node only publishes when theres an x and y for each node assigned, otherwise short circuit loop. callback is event triggered so assumption is that beacon  5 will publish in 1 event and beacon 6 will publish in the next event.

--- pose_calulator.py
+++ pose_calulator.py
@@ -9,35 +9,40 @@ import math
 
 def callback1(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    z = data.data.split(',')
-    x1 = float(z[][])    #Fill with appropriate data cells of node data
-    y1 = float(z[][])    #Fill with appropriate data cells of node data
+    if counter == 0: #if first values not assigned:
+        z = data.data.split(',')
+        x1 = float(z[][])    #Fill with appropriate data cells of node data
+        y1 = float(z[][])    #Fill with appropriate data cells of node data
+        counter = counter + 1
+    else:           #assign second values
+    	z = data.data.split(',')
+        x2 = float(z[][])    #Fill with appropriate data cells of node data
+        y2 = float(z[][])    #Fill with appropriate data cells of node data
+        counter = 0
+        ready = True
 
-def callback2(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    z = data.data.split(',')
-    x2 = float(z[][])    #Fill with appropriate data cells of node data
-    y2 = float(z[][])    #Fill with appropriate data cells of node data
+
+
 def pose():
     rospy.init_node('calculator', anonymous = True)
-    rospy.Subscriber("hedge1", String, callback1)  #Replace hedge 1 and 2 with actual ids...
-    rospy.Subscriber("hedge2", String, callback2)
+    rospy.Subscriber("hedge", String, callback1)  #Replace hedge 1 and 2 with actual ids...
     pub = rospy.Publisher("robot_pose", Float32)
 
-    rospy.rate(10)     #Sets rate of pose to 10 Hz
-    if x2 >= x1 and y2 >= y1 :  #Quadrant1 output [0-90]
-        pose = math.atan( (y2 - y1)/(x2 - x1) )
-    if x2 >= x1 and y2 < y1:  #Quadrant4 output [270,360)
-        pose = 360 + math.atan( (y2 - y1)/(x2 - x1) )    
-    if x2 < x1 and y2 >= y1:   #Quadrant2 output (90,180]
-        pose = 180 + math.atan( (y2 - y1)/(x2 - x1) ) 
-    if x2 < x1 and y2 < y1:    #Quadrant3 output (180,270)
-    	pose = 180 + math.atan( (y2 - y1)/(x2 - x1) )
+    rospy.rate(250)     #Sets rate of pose to 250 Hz faster than hedge output single message rate. only publishes when boolean is true
 
-    
-    pub.publish(pose)
+    if counter == 0 and ready is True:
+    	ready = False
+        if x2 >= x1 and y2 >= y1 :  #Quadrant1 output [0-90]
+            pose = math.atan( (y2 - y1)/(x2 - x1) )
+        if x2 >= x1 and y2 < y1:  #Quadrant4 output [270,360)
+            pose = 360 + math.atan( (y2 - y1)/(x2 - x1) )    
+        if x2 < x1 and y2 >= y1:   #Quadrant2 output (90,180]
+            pose = 180 + math.atan( (y2 - y1)/(x2 - x1) ) 
+        if x2 < x1 and y2 < y1:    #Quadrant3 output (180,270)
+    	    pose = 180 + math.atan( (y2 - y1)/(x2 - x1) )
+        pub.publish(pose)
+
     rate.sleep()
-
     rospy.spin()    #loops
 if __name__ == '__main__':
    x1 = 0
@@ -45,4 +50,5 @@ if __name__ == '__main__':
    y1 = 0
    y2 = 0
    pose = 0.0
+   ready = False
    pose()
